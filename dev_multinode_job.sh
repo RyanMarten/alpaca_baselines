3258d5328ee0b349680cf811af3696cb30f8957a
Ryan Marten <rmarten@login2.ls6.tacc.utexas.edu>
2024-08-16 04:01:39 -0500

    ok now trying it with torchrun

--- dev_multinode_job.sh
+++ dev_multinode_job.sh
@@ -44,31 +44,31 @@ srun $WORK/dcft/stanford_alpaca/venv/bin/python -c "import torch; print('Torch v
 
 # Run the training script
 srun echo "Starting torchrun command..."
-#srun $WORK/dcft/stanford_alpaca/venv/bin/torchrun \
-#    --nnodes=$SLURM_NNODES \
-#    --nproc_per_node=$GPUS_PER_NODE \
-#    --nnodes=$SLURM_NNODES \
-#    --node_rank=$SLURM_PROCID \
-#    --master_addr=$MASTER_ADDR \
-#    --master_port=$MASTER_PORT \ 
-#    $WORK/dcft/stanford_alpaca/train.py \
-#    --model_name_or_path $WORK/dcft/llama-7b \
-#    --data_path ./alpaca_data.json \
-#    --bf16 True \
-#    --output_dir $WORK/dcft/llama-7b-checkpoints \
-#    --num_train_epochs 3 \
-#    --per_device_train_batch_size 4 \
-#    --per_device_eval_batch_size 4 \
-#    --gradient_accumulation_steps 8 \
-#    --evaluation_strategy "no" \
-#    --save_strategy "steps" \
-#    --save_steps 2000 \
-#    --save_total_limit 1 \
-#    --learning_rate 2e-5 \
-#    --weight_decay 0. \
-#    --warmup_ratio 0.03 \
-#    --lr_scheduler_type "cosine" \
-#    --logging_steps 1 \
-#    --fsdp "full_shard auto_wrap offload" \
-#    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
-#    --tf32 True
+srun $WORK/dcft/stanford_alpaca/venv/bin/torchrun \
+    --nnodes=$SLURM_NNODES \
+    --nproc_per_node=$GPUS_PER_NODE \
+    --nnodes=$SLURM_NNODES \
+    --node_rank=$SLURM_PROCID \
+    --master_addr=$MASTER_ADDR \
+    --master_port=$MASTER_PORT \ 
+    $WORK/dcft/stanford_alpaca/train.py \
+    --model_name_or_path $WORK/dcft/llama-7b \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir $WORK/dcft/llama-7b-checkpoints \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap offload" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True
