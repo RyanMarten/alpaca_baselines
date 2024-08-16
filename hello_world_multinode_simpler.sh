6c1c2fd69a9187ca3c9363ba0f1c384ebe16547e
Ryan Marten <rmarten@login2.ls6.tacc.utexas.edu>
2024-08-16 03:12:43 -0500

    test what happens when ntaskspernode is not specified

--- hello_world_multinode_simpler.sh
+++ hello_world_multinode_simpler.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --partition=gpu-a100-dev
-#SBATCH --job-name=hello-multinode-gpu
+#SBATCH --partition=development
+#SBATCH --job-name=hello-multinode
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
 #SBATCH -t 0:01:00
 #SBATCH --output=logs/%x_%j.out
 
 echo "Job started at $(date)"
-echo "SLURM_NODELIST: $SLURM_NODELIST"
+#echo "SLURM_NODELIST: $SLURM_NODELIST"
+#echo "SLURM_JOB_NUM_NODES: $SLURM_JOB_NUM_NODES"
+env | grep SLURM
 
+srun hostname
 srun bash -c 'echo "Hello world from $(hostname) (SLURM_PROCID: $SLURM_PROCID)"'
-srun nvidia-smi
 
 echo "Job ended at $(date), reported by $SLURMD_NODENAME with PROCID $SLURM_PROCID"
 
