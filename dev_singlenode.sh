7a5282975292890b7c32e31109f4e2caf6ba8483
Ryan Marten <rmarten@login2.ls6.tacc.utexas.edu>
2024-08-16 04:18:32 -0500

    indentical, but now single node to remove variables

--- dev_singlenode.sh
+++ dev_singlenode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu-a100-dev
-#SBATCH --job-name=dev-debug
-#SBATCH --nodes=2
+#SBATCH --job-name=dev-dcft-alpaca-singlenode
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -t 00:30:00
 #SBATCH --output=logs/%x_%j.out
@@ -42,9 +42,33 @@ srun bash -c 'echo "Hello world from $SLURMD_NODENAME (SLURM_PROCID: $SLURM_PROC
 srun $WORK/dcft/stanford_alpaca/venv/bin/python -c "import torch; print('Torch version:', torch.__version__)"
 srun $WORK/dcft/stanford_alpaca/venv/bin/python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}, Device count: {torch.cuda.device_count()}')"
 
-# ensuring you can see hostnode
-srun ping -c 3 $MASTER_ADDR
-
 # Run the training script
-srun echo "Starting debug python script..."
-srun $WORK/dcft/stanford_alpaca/venv/bin/python $WORK/dcft/stanford_alpaca/debug_distributed.py
+srun echo "Starting torchrun command..."
+srun $WORK/dcft/stanford_alpaca/venv/bin/torchrun \
+    --nnodes=$SLURM_NNODES \
+    --nproc_per_node=$GPUS_PER_NODE \
+    --nnodes=$SLURM_NNODES \
+    --node_rank=$SLURM_PROCID \
+    --master_addr=$MASTER_ADDR \
+    --master_port=$MASTER_PORT \ 
+    $WORK/dcft/stanford_alpaca/train.py \
+    --model_name_or_path $WORK/dcft/llama-7b \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir $WORK/dcft/llama-7b-checkpoints \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap offload" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True
