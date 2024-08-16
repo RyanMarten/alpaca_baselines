27159336efcca3a5fb701d1c2a575dcd9c08a2f3
Ryan Marten <rmarten@login2.ls6.tacc.utexas.edu>
2024-08-16 00:42:46 -0500

    trying claudes suggesting to force execution on multiple nodes

--- hello_world_multinode.sh
+++ hello_world_multinode.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 #SBATCH --partition=gpu-a100-dev
 #SBATCH --job-name=hello-multinode
-#SBATCH --nodes=2  # Specify the number of nodes you want to use
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH -t 0:30:00
-#SBATCH --output=logs/%x_%j_%t.out
-#SBATCH --account=CCR23021
-#SBATCH --mail-type=all
-#SBATCH --mail-user=marten4@illinois.edu
+#SBATCH --output=logs/%x_%j_%N_%t.out
 
-srun echo "Hello world from node $(hostname) with Slurm proc ID $SLURM_PROCID"
+echo "Job started at $(date)"
+echo "SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
+echo "SLURM_NODELIST: $SLURM_NODELIST"
 
-# Set the master node's address (usually the first node in the allocation)
-#export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
-#export MASTER_PORT=29500
-#
-#echo "Show hostnames $(scontrol show hostnames $SLURM_JOB_NODELIST | tr '\n' ' ')"
-#
-#export NODE_RANK=$SLURM_NODEID
-#export SLURM_GPUS_PER_NODE=2
-#
-#echo "SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
-#echo "SLURM_NNODES: $SLURM_NNODES"
-#echo "SLURM_GPUS_PER_NODE: $SLURM_GPUS_PER_NODE"
-#echo "SLURM_JOB_ID: $SLURM_JOB_ID"
-#echo "SLURM_PROCID: $SLURM_PROCID"
-#echo "MASTER_ADDR: $MASTER_ADDR"
-#echo "MASTER_PORT: $MASTER_PORT"
-#
-#echo "Node: $(hostname)"
+# Force execution on all allocated nodes
+srun --nodes=2 --ntasks=2 hostname
+srun --nodes=2 --ntasks=2 echo "Hello world from \$SLURMD_NODENAME (SLURM_PROCID: \$SLURM_PROCID)"
+
+# Optional: Add more commands to demonstrate multi-node execution
+srun --nodes=2 --ntasks=2 sleep 2
+srun --nodes=2 --ntasks=2 date
+
+echo "Job ended at $(date)"
+
+# Print the contents of both log files
+echo "Contents of log files:"
+cat logs/hello-multinode_${SLURM_JOB_ID}_*.out
