7b968079b4ccf73fa11900d7664f127d01922619
Ryan Marten <rmarten@login2.ls6.tacc.utexas.edu>
2024-08-17 00:58:37 -0500

    update tacc training script with new master node setup

--- train_tacc.sh
+++ train_tacc.sh
@@ -7,34 +7,39 @@
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --account=CCR23021
 
-# Set up environment variables for multinode training
-nodes=($(scontrol show hostname $SLURM_NODELIST))
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-export MASTER_ADDR=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
-export MASTER_PORT=25678
-export NODE_RANK=$SLURM_NODEID
-export GPUS_PER_NODE=2
-
-# Debug output
+# Job info
+echo "Job started at $(date)"
+echo "SLURM_JOB_ID: $SLURM_JOB_ID"
 echo "SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
 echo "SLURM_NNODES: $SLURM_NNODES"
-echo "GPUS_PER_NODE: $GPUS_PER_NODE"
-echo "SLURM_JOB_ID: $SLURM_JOB_ID"
+
+# Networking between nodes to master node
+
+# From srun docs: "Slurm runs a single copy of the batch script on the first node in the set of allocated nodes"
+# This first node will be the master node and we will set an environment variable for all the nodes to use
+# Therefore this will show PROCID 0 and the ip address of the master node
+export MASTER_ADDR=$(hostname --ip-address)
+export MASTER_PORT=25678
 echo "SLURM_PROCID: $SLURM_PROCID"
 echo "MASTER_ADDR: $MASTER_ADDR"
 echo "MASTER_PORT: $MASTER_PORT"
-echo "hostname on node running the sbatch: $(hostname)"
-echo "all hostnames $(scontrol show hostnames $SLURM_JOB_NODELIST | tr '\n' ' ')"
-srun bash -c 'echo "Hello world from $SLURMD_NODENAME (SLURM_PROCID: $SLURM_PROCID) (SLURM_NODEID: $SLURM_NODEID)"'
 
-# ensuring the environment is available
+# Each node in the list should print their own process id (used as node_rank) and the same master networking info
+echo "All nodes Hostnames"
+echo "$(scontrol show hostname $SLURM_JOB_NODELIST)"
+srun bash -c 'echo "Hello world from $(hostname) (SLURM_PROCID: $SLURM_PROCID). I see MASTER_ADDR:MASTER_PORT as $MASTER_ADDR:$MASTER_PORT"'
+
+# Additional environment variables
+export GPUS_PER_NODE=2
+echo "GPUS_PER_NODE: $GPUS_PER_NODE"
+
+# Ensure torch and cuda and devices are available
 srun $WORK/dcft/stanford_alpaca/venv/bin/python -c "import torch; print('Torch version:', torch.__version__)"
 srun $WORK/dcft/stanford_alpaca/venv/bin/python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}, Device count: {torch.cuda.device_count()}')"
 
 # Run the training script
 srun echo "Starting torchrun command..."
-srun ./venv/bin/torchrun --rdzv-id=$SLURM_JOB_ID --rdzv-backend=c10d --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT --master_addr=$MASTER_ADDR --node_rank=$SLURM_PROCID --nnodes=$SLURM_NNODES --nproc_per_node=auto --master_port=12345 train.py \
+srun ./venv/bin/torchrun --rdzv-id=$SLURM_JOB_ID --rdzv-backend=c10d --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT --master_addr=$MASTER_ADDR --node_rank=$SLURM_PROCID --nnodes=$SLURM_NNODES --nproc_per_node=auto --master_port=$MASTER_PORT train.py \
     --model_name_or_path $WORK/dcft/llama-7b \
     --data_path ./alpaca_data.json \
     --bf16 False \
@@ -56,3 +61,5 @@ srun ./venv/bin/torchrun --rdzv-id=$SLURM_JOB_ID --rdzv-backend=c10d --rdzv-endp
     --fsdp "full_shard auto_wrap" \
     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
     --tf32 True
+
+echo "Job ended at $(date)"
