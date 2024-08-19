28b60249724483c942cf2b9f1ba6afdea21130e0
ryanmarten <ryanmarten2000@gmail.com>
2024-08-19 10:56:13 -0700

    only add top score example

--- data_generation_fast/distributed_rouge.py
+++ data_generation_fast/distributed_rouge.py
@@ -97,8 +97,8 @@ def filter_rouge(input_file: str, output_file: str):
                     "input": input_text,
                     "output": output_text,
                     "filtered_reason": "rouge_similarity",
-                    "scores": max_scores,
-                    "similar_instructions": max_instructions
+                    "score": max_scores[np.argmax(max_scores)],
+                    "similar_instruction": max_instructions[np.argmax(max_scores)]
                 })
             else:
                 not_filtered.append({"instruction": inst, "input": input_text, "output": output_text})
