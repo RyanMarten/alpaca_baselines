93170b6ff74b405ca6fd0049d1e725cce5e38450
ryanmarten <ryanmarten2000@gmail.com>
2024-08-23 08:42:54 -0700

    increase per device batch size, decrease gradient accumulation steps for train_sf.sh

--- train_sf.sh
+++ train_sf.sh
@@ -1,6 +1,6 @@
 
 MODEL_PATH=~/llama-7b
-OUTPUT_DIR="~/alpaca-checkpoints/8xH100-gradient-accumulation-steps4"
+OUTPUT_DIR="~/alpaca-checkpoints/8xH100-gradient-accumulation-steps1"
 MASTER_PORT=41592
 
 mkdir -p $OUTPUT_DIR
@@ -11,9 +11,8 @@ torchrun --nproc_per_node=8 --master_port=$MASTER_PORT train.py \
     --bf16 True \
     --output_dir $OUTPUT_DIR \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 16 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 2000 \
